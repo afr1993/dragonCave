129b6c20cbf5171c03568ad8b8ea9a8e25b57443
afr1993 <afr.1993@gmail.com>
2025-04-25 05:14:30 -0700

    First commit: First message and scanner

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+
+public class Main {
+    public static void main(String[] args) {
+
+        java.util.Scanner scanner = new java.util.Scanner(System.in);
+
+        System.out.println("You are in a land full of dragons. In front of you,\n" +
+                "You see two caves. In one cave, the dragon is friendly\n" +
+                "and will share his treasure with you. The other dragon\n" +
+                "is greedy and hungry and will eat you on sight.\n" +
+                "Which cave will you go into? (1 or 2) :");
+
+        int option = scanner.nextInt();
+
+    }
+}
